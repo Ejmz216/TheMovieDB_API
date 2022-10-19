b9ef4e1503d7bc3a4233af3ea0d06d5192db6ae1
Elmer Jose Muñoz Zuñiga <67802793+Ejmz216@users.noreply.github.com>
2022-10-18 21:00:38 -0500

    Commit 7: Layout 3

--- src/main.js
+++ src/main.js
@@ -7,13 +7,27 @@ const api = axios.create({
         'api_key': API_KEY,
     },
 });
+async function getPosterImg() {
+    const { data } = await api('collection/9735/images');
+    const collections = data.backdrops;
+    console.log(collections)
+
+        const headerImageContainer = document.querySelector('#header .header-image-container');
+        const headertitleP = document.querySelector('#header .header-title--principal');
+        const posterImage = document.createElement('img');
+        posterImage.classList.add('poster-img');
+        posterImage.setAttribute('src', 'https://image.tmdb.org/t/p/original' + collections[4].file_path);        
+
+        headerImageContainer.insertBefore(posterImage, headertitleP);
+}
 
 async function getTrendingMoviesPreview() {
-    const {data} = await api('trending/movie/day');
+    const { data } = await api('trending/movie/day');
     const movies = data.results;
-    
+
     movies.forEach((movie) => {
         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
 
@@ -22,15 +36,20 @@ async function getTrendingMoviesPreview() {
         movieImg.setAttribute('alt', movie.title);
         movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
 
+        const movieTitle = document.createElement('h1');
+        movieTitle.classList.add('movie-titlePreview');
+        const movieTitleText = document.createTextNode(movie.title);
+
         movieContainer.appendChild(movieImg);
+        movieTitle.appendChild(movieTitleText);
+        movieContainer.appendChild(movieTitle);
         trendingPreviewMoviesContainer.appendChild(movieContainer);
     });
 }
 
 async function getCategoriesPreview() {
-    const {data} = await api('genre/movie/list');
+    const { data } = await api('genre/movie/list');
     const categories = data.genres;
-
     categories.forEach((category) => {
         const categoriesPreviewContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
 
@@ -38,7 +57,7 @@ async function getCategoriesPreview() {
         categoryContainer.classList.add('category-container');
 
         const categoryTitle = document.createElement('h3');
-        categoryTitle.classList.add('category-title'); 7
+        categoryTitle.classList.add('category-title');
 
         categoryTitle.setAttribute('id', 'id' + category.id);
         const categoryTitleText = document.createTextNode(category.name);
@@ -49,5 +68,6 @@ async function getCategoriesPreview() {
     });
 }
 
+getPosterImg();
 getTrendingMoviesPreview();
 getCategoriesPreview();
